ed6eb10873084821ba77ec41a1bc340b1c167215
Leonardo Moraes <demoraesleonardo327@gmail.com>
2026-07-04 10:18:36 -0300

    atualizar testes com ferramenta pytest

--- testes/test_presencas_dia.py
+++ testes/test_presencas_dia.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import sync_playwright 
+from playwright.sync_api import sync_playwright,expect
 import time
 import os
 
@@ -12,8 +12,9 @@ def gerar_txt(valor):
 
     with open(caminho_arquivo, "a", encoding="utf-8") as arquivo:
         arquivo.write(valor + "\n")
-emails=["joao@gmail.com","duda@gmail.com","joana@gmail.com"]
-with sync_playwright() as p:
+def test():
+ emails=["joao@gmail.com","duda@gmail.com","joana@gmail.com"]
+ with sync_playwright() as p:
           browser=p.chromium.launch(headless=False)
           pagina=browser.new_page()
           for email in emails:
@@ -21,8 +22,10 @@ with sync_playwright() as p:
              pagina.fill("[name='email']",email)     
              pagina.fill("[name='senha']","123")
              pagina.get_by_role('button',name="Entrar").click()
-             pagina.get_by_role('button',name=" Confirmar presença").click()
+             pagina.locator('[name="acao"]').click()
              gerar_txt(email)
              print(f'testado email de {email}')
 
           browser.close()
+if __name__=="__main__":
+        test()
